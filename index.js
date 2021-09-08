fb9ee8799fb5a9e02ef9e68e4a2801c62d944807
Aleksey Mamaev <a.mamaevs@gmail.com>
2021-09-08 16:54:36 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function bubbleSort(arr) {
+    for (var i = 0, endI = arr.length - 1; i < endI; i++) {
+        var wasSwap = false;
+        for (var j = 0, endJ = endI - i; j < endJ; j++) {
+            if (arr[j] > arr[j + 1]) {
+                var swap = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = swap;
+                wasSwap = true;
+            }
+        }
+        if (!wasSwap) break;
+    }
+    return arr;
+}
